eee477ee4b9206c44f0339106a6ad80abadc668a
Adam Oumar Abdel-rahman <adam92oumar@gitlab.com>
2023-05-15 13:37:18 +0200

    Fix mistake in content_filtering.Dockerfile

--- docker/build.sh
+++ docker/build.sh
@@ -4,6 +4,7 @@ if [[ "$1" = "cf" ]]; then
     echo "Build OpenSSL image with content filtering extesion."
     DOCKERFILE="content_filtering.Dockerfile"
     IMAGE="presto-content-filtering-tls:openssl-3.0.1"
+    # git -C ../ diff > content_filtering_extension.patch
 else
     echo "Construct OpenSSL image"
     DOCKERFILE="openssl.Dockerfile"
